77a1567235f93878d1a464db06f41479635e9481
hankei6km <gh-hankei6km@outlook.jp>
2021-03-03 15:43:27 +0900

    Load edited HTML immediately

--- types/client/api/v1/docs/_id@string/index.ts
+++ types/client/api/v1/docs/_id@string/index.ts
@@ -16,16 +16,27 @@ export default mockMethods<Methods>({
     // stub 使いたいが、どの辺が production に残るかわからないので
     // とりあえずコードを書く.
     const idx = contents.findIndex(({ id }) => id === values.id);
-    return idx >= 0
-      ? {
-          status: 200,
-          resHeaders: {},
-          resBody: contents[idx]
-        }
-      : {
-          status: 404,
-          resHeaders: {},
-          resBody: {}
-        };
+    if (idx < 0) {
+      return {
+        status: 404,
+        resHeaders: {},
+        resBody: {}
+      };
+    }
+    const resBody = { ...contents[idx] };
+    if (
+      process.env.NODE_ENV === 'development' &&
+      (resBody.id === 'try-it' || resBody.id === 'try-it-demo')
+    ) {
+      resBody.html = require('fs')
+        .readFileSync('docs/try-it.html')
+        .toString('utf-8')
+        .replace(/\n/g, '');
+    }
+    return {
+      status: 200,
+      resHeaders: {},
+      resBody
+    };
   }
 });
